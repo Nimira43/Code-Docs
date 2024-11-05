e2934bbeaa994c4bf685b0cb355fd131da380d5a
Nimira43 <n.rabone72@aol.com>
2024-11-05 19:50:50 +0000

    Styling work area

--- 2-Transpiling-Code-Demo/src/App.tsx
+++ 2-Transpiling-Code-Demo/src/App.tsx
@@ -1,9 +1,17 @@
 const App = () => {
   return (
-    <div className='main-container'>
-      <h1>Transpiling Demo using ESBuild</h1>
+    <>
+      <div className='main-container'>
+        <h1>Transpiling Demo using ESBuild</h1>
+        <div className='sub-container'>
+        <textarea></textarea>
+          <div className='btn-box'>
+            <button>Submit</button>
+        </div> 
+      </div>
       <h1 className='logo'>NimiraTech</h1>
-    </div>
-  )
+      </div>
+    </>
+      )
 }
 export default App
