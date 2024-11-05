ae3d7a2e68396efd79da891a2cde1175f89565fb
Nimira43 <n.rabone72@aol.com>
2024-11-05 20:33:06 +0000

    Last of CSS done. added. Brought in useState to handle code input and code output - in the console at the moment. The interfac is done though

--- 2-Transpiling-Code-Demo/src/App.tsx
+++ 2-Transpiling-Code-Demo/src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const onClick = () => {
+    console.log(input)
+  }
+
   return (
     <>
       <div className='main-container'>
         <h1>Transpiling Demo using ESBuild</h1>
         <div className='sub-container'>
-          <textarea></textarea>
-          <button>Submit</button>
+          <textarea 
+            value={input}
+            onChange={e => setInput(e.target.value)}
+          >
+          </textarea>
+          <button onClick={onClick}>Submit</button>
         </div>  
-      <h1 className='logo'>NimiraTech</h1>
+        <pre>{code}</pre>
+        <h1 className='logo'>NimiraTech</h1>
       </div>
     </>
       )
