1fc5578959689a47a409458adf9a1ef3f9460282
Nimira43 <n.rabone72@aol.com>
2025-02-12 22:27:28 +0000

    Incorporating resolveDir and dynamically constructing the URL for relative paths makes the plugin more robust and capable of handling nested dependencies effectively.

--- 2-Transpiling-Code-Demo/src/plugins/unpkg-path-plugin.tsx
+++ 2-Transpiling-Code-Demo/src/plugins/unpkg-path-plugin.tsx
@@ -14,7 +14,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href
           }
         }
 
