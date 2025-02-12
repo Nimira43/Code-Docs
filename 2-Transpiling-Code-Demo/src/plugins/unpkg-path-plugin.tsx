b407c772abd6b869495d5a1e26a96b2f1654fe41
Nimira43 <n.rabone72@aol.com>
2025-02-12 22:44:05 +0000

    ..trying React-Dom

--- 2-Transpiling-Code-Demo/src/plugins/unpkg-path-plugin.tsx
+++ 2-Transpiling-Code-Demo/src/plugins/unpkg-path-plugin.tsx
@@ -34,7 +34,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('react');
+              const message = require('react-dom');
               console.log(message);
             `
           }
