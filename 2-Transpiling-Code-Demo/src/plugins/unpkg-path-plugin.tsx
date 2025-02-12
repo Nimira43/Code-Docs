290f9f88f2127e8d20a67e75dc033c9bad368917
Nimira43 <n.rabone72@aol.com>
2025-02-12 20:47:31 +0000

    Adding Axxios to fettch data from URLs

--- 2-Transpiling-Code-Demo/src/plugins/unpkg-path-plugin.tsx
+++ 2-Transpiling-Code-Demo/src/plugins/unpkg-path-plugin.tsx
@@ -26,6 +26,8 @@ export const unpkgPathPlugin = () => {
             `
           }
         } 
+        const { data } = await axios.get(args.path)
+        console.log(data)
       }) 
     }
   }
