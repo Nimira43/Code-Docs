e9aeca4e77ef006b22448877a39b63e6776dd9d1
Nimira43 <n.rabone72@aol.com>
2025-02-13 04:57:46 +0000

    Moved the common caching logic into its own build.onLoad.

--- 3-Caching/src/plugins/fetch-plugin.tsx
+++ 3-Caching/src/plugins/fetch-plugin.tsx
@@ -18,13 +18,15 @@ export const fetchPlugin = (inputCode: string) => {
         }
       })
 
-      build.onLoad({ filter: /.css$/ }, async(args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
         
         if (cachedResult) {
           return cachedResult
         }
-        
+      })
+
+      build.onLoad({ filter: /.css$/ }, async(args: any) => {  
         const { data, request } = await axios.get(args.path)
         const escaped = data
           .replace(/\n/g, '')
@@ -46,19 +48,13 @@ export const fetchPlugin = (inputCode: string) => {
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
-        
-        if (cachedResult) {
-          return cachedResult
-        }
-        
         const { data, request } = await axios.get(args.path)
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
-        
+
         await fileCache.setItem(args.path, result)
         return result
       }) 
