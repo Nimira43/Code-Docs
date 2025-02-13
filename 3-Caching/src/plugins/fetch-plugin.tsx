9b5b3c8809883c6192a27e6531fff429f771536c
Nimira43 <n.rabone72@aol.com>
2025-02-13 03:40:15 +0000

    Update to handle escaping characters in CSS files to ensure proper injection into the document.

--- 3-Caching/src/plugins/fetch-plugin.tsx
+++ 3-Caching/src/plugins/fetch-plugin.tsx
@@ -26,16 +26,23 @@ export const fetchPlugin = (inputCode: string) => {
         // }
         
         const { data, request } = await axios.get(args.path)
-
-        console.log(args.path)
-
         const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
-        const contents = fileType === 'css' ? 
-          `
-            const style = document.createElement('style')
-            style.innerText = 'body { background-color: "red" } '
-            document.head.appendChild(style)
-          ` : data
+        
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'")
+
+        const contents =
+          fileType === 'css'
+            ? 
+              `
+                const style = document.createElement('style')
+                style.innerText = '${escaped}'
+                document.head.appendChild(style)
+              `
+            :
+              data
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
