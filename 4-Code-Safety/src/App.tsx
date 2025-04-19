4d92ce3c300ebbb35ea659fe3be18a0b230af758
Nimira43 <n.rabone72@aol.com>
2025-04-19 05:53:32 +0100

    Console logging current code.

--- 4-Code-Safety/src/App.tsx
+++ 4-Code-Safety/src/App.tsx
@@ -7,13 +7,14 @@ const App = () => {
   const ref = useRef<any>()
   const iframe = useRef<any>()
   const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
+  // const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     })
+    console.log("Esbuild service started!");
   } 
 
   useEffect(() => {
@@ -21,9 +22,12 @@ const App = () => {
   }, [])
 
   const onClick = async () => {
+    console.log("Submit button clicked!")
     if (!ref.current) {
       return
     }
+
+    iframe.current.srcdoc = html
 
     const result = await ref.current.build({
       entryPoints: ['index.js'],
@@ -38,7 +42,9 @@ const App = () => {
         global: 'window'
       }
     })
+    console.log(result);
     iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    console.log("Posted to iframe:", result.outputFiles[0].text);
   } 
 
   const html = `
@@ -48,9 +54,11 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
+            console.log('Message received:', event.data);
             try {
               eval(event.data)
             } catch (err) {
+              console.error("Runtime error:", err)
               const root = document.querySelector('#root')
               root.innerHTML = '<div style="color: red;"><h4>Runtime Error: </h4>' + err + '</div>'
               throw err
@@ -75,7 +83,7 @@ const App = () => {
           <button onClick={onClick}>Submit</button>
         </div>  
         <div className='code-container'> 
-          <pre>{code}</pre> 
+          {/* <pre>{code}</pre>  */}
           <iframe
             ref={iframe}
             title="Code Execution Preview"
