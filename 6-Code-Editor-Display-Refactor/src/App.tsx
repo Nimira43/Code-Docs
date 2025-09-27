7081e65b992da1aab7bdc5c1a4a9c1a8f9cb3975
Nimira43 <n.rabone72@aol.com>
2025-09-27 04:51:14 +0100

    bundler refactor - 3

--- 6-Code-Editor-Display-Refactor/src/App.tsx
+++ 6-Code-Editor-Display-Refactor/src/App.tsx
@@ -11,10 +11,7 @@ const App = () => {
   const [input, setInput] = useState('')
 
   const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    })
+    ref.current = 
   } 
 
   useEffect(() => {
--- 6-Code-Editor-Display-Refactor/src/bundler/index.ts
+++ 6-Code-Editor-Display-Refactor/src/bundler/index.ts
@@ -2,6 +2,9 @@ import * as esbuild from 'esbuild-wasm'
 import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin'
 import { fetchPlugin } from '../plugins/fetch-plugin'
 
-export default (rawCode: string) => {
-  
+export default async (rawCode: string) => {
+  await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+    })
 }
