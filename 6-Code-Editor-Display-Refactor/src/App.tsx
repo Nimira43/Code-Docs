79dbf3dd3233591685185a8005e6001a3aab5e6a
Nimira43 <n.rabone72@aol.com>
2025-10-12 03:11:00 +0100

    Cleaned up App.tsx

--- 6-Code-Editor-Display-Refactor/src/App.tsx
+++ 6-Code-Editor-Display-Refactor/src/App.tsx
@@ -1,29 +1,10 @@
 import 'bulmaswatch/solar/bulmaswatch.min.css'
 import CodeCell from './components/code-cell'
 
-
 const App = () => {
-  const [code, setCode] = useState('')
-  const [input, setInput] = useState('')
-
-  const onClick = async () => {
-    const output = await bundle(input)
-    console.log('Bundled Output:', output)
-    setCode(output)
-  }
-
   return (
     <div>
-      <CodeEditor 
-        initialValue='const a = 9'
-        onChange={(value) => setInput(value)}  
-      />
-      <div>  
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview 
-        code={code}
-      /> 
+      <CodeCell />
     </div>
   )
 }
