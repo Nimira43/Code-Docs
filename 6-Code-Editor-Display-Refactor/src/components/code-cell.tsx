26a91a8be10d9d04789cb773325066174a95a180
Nimira43 <n.rabone72@aol.com>
2025-10-12 02:58:18 +0100

    Copied the entire contents of App.tsx and placed inside code-cell.tsx

---
+++ 6-Code-Editor-Display-Refactor/src/components/code-cell.tsx
@@ -0,0 +1,32 @@
+import 'bulmaswatch/solar/bulmaswatch.min.css'
+import { useState } from 'react'
+import CodeEditor from './components/code-editor'
+import Preview from './components/preview'
+import bundle from './bundler'
+
+const App = () => {
+  const [code, setCode] = useState('')
+  const [input, setInput] = useState('')
+
+  const onClick = async () => {
+    const output = await bundle(input)
+    console.log('Bundled Output:', output)
+    setCode(output)
+  }
+
+  return (
+    <div>
+      <CodeEditor 
+        initialValue='const a = 9'
+        onChange={(value) => setInput(value)}  
+      />
+      <div>  
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <Preview 
+        code={code}
+      /> 
+    </div>
+  )
+}
+export default App
