c7410fb4ffdcdd86cdf413a73b640cffa5e75c6b
Nimira43 <n.rabone72@aol.com>
2025-10-28 01:20:59 +0000

    Wrapped returned JSX inside Resizable

--- 6-Code-Editor-Display-Refactor/src/components/code-cell.tsx
+++ 6-Code-Editor-Display-Refactor/src/components/code-cell.tsx
@@ -14,18 +14,21 @@ const CodeCell = () => {
   }
 
   return (
-    <div>
-      <CodeEditor 
-        initialValue='const a = 1'
-        onChange={(value) => setInput(value)}  
-      />
-      <div>  
-        <button onClick={onClick}>Submit</button>
+    <Resizable direction='vertical'>
+      <div>
+        <CodeEditor 
+          initialValue='const a = 1'
+          onChange={(value) => setInput(value)}  
+        />
+        <div>  
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview 
+          code={code}
+        /> 
       </div>
-      <Preview 
-        code={code}
-      /> 
-    </div>
+    </Resizable>
+    
   )
 }
 export default CodeCell
