5f706887171fb7b5d60aae053105a95655698eef
Nimira43 <n.rabone72@aol.com>
2025-12-02 13:30:46 +0000

    Created Rebuild folder. Archived current code. New package.json with new deps

---
+++ Rebuild/archive/src/App.tsx
@@ -0,0 +1,10 @@
+import CodeCell from './components/code-cell'
+
+const App = () => {
+  return (
+    <div>
+      <CodeCell />
+    </div>
+  )
+}
+export default App
---
+++ Rebuild/archive/src/components/preview.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from 'react'
+import './preview.css'
+
+interface PreviewProps {
+  code: string
+}
+
+const html = `
+    <html>
+      <head>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data)
+            } catch (err) {
+              const root = document.querySelector('#root')
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error: </h4>' + err + '</div>'
+              // throw err
+              console.error(err)
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `
+
+const Preview = ({ code }: PreviewProps) => {
+  const iframe = useRef<any>()
+
+  useEffect(() => {
+    const iframeEl = iframe.current
+    iframeEl.srcdoc = html
+
+    const handleLoad = () => {
+      iframeEl.contentWindow.postMessage(code, '*')
+    }
+
+    iframeEl.addEventListener('load', handleLoad)
+
+    return () => {
+      iframeEl.removeEventListener('load', handleLoad)
+    }
+  }, [code])
+
+  return (
+    <div className='preview-wrapper'>
+      <iframe 
+        ref={iframe}
+        title="Code Execution Preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  )
+}
+
+export default Preview
